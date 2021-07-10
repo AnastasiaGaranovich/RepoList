fc971f4e2a70372917b296d5791bc492f153f396
AnastasiaGaranovich <nastyamadness@gmail.com>
2021-07-10 14:28:28 +0300

    Add Main and UserDeatails view controllers

---
+++ RepoList/UserDetailsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  UserDetailsViewController.swift
+//  RepoList
+//
+//  Created by Анастасия Гаранович on 10.07.2021.
+//
+
+import UIKit
+
+class UserDetailsViewController: UITableViewController {
+	
+}
